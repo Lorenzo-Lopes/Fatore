4daca4f3b9d57d891bb52541a81485f6a5223419
lorenzo-lopes <lorenzolopes.dev@gmail.com>
2025-08-29 15:05:14 -0300

    Criado ex 1 e 2

---
+++ Exercicio/Aula_01/ex_01.py
@@ -0,0 +1,6 @@
+#Faça um código que possua as variáveis  Nome, Idade, Cidade. Preencha elas e em seguida utilizando o comando Print, coloque todas as informações na tela concatenando elas utilizando o FString
+
+nome = "Lorenzo Lopes"
+idade = 26
+cidade = "Piracicaba" 
+print(f"Olá, tudo bem ? Me chamo {nome} tenho {idade} anos e atualmente moro em {cidade}")
---
+++ Exercicio/Aula_01/ex_02.py
@@ -0,0 +1,6 @@
+primeiro_numero = int(input("Digite um numero: "))
+segundo_numero = int(input("Digite outro numero: "))
+
+media = (primeiro_numero+segundo_numero) / 2
+
+print(f"A media dos numero : {primeiro_numero:.1f} e {segundo_numero:.1f}. Resulta em : {media:.1f}")
