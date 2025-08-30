75b2792575f93dcdd0702e6e2cab319e6ea12461
lorenzo-lopes <lorenzolopes.dev@gmail.com>
2025-08-30 08:46:30 -0300

    Criado pasta aula dois add ex_01

---
+++ Exercicio/Aula_02/ex_01.py
@@ -0,0 +1,10 @@
+# Crie uma lista com 5 nomes de amigos e exiba cada nome.
+
+
+lista_nomes = ["joao","ana","clara","ana eleisa","Lorenzo"]
+
+print(lista_nomes[0])
+print(lista_nomes[1])
+print(lista_nomes[2])
+print(lista_nomes[3])
+print(lista_nomes[4])
