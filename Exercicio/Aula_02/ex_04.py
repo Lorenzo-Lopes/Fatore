873154324bc69d16279410f7a85abc50b1f5d740
lorenzo-lopes <lorenzolopes.dev@gmail.com>
2025-08-30 15:42:23 -0300

    criando ex4

---
+++ Exercicio/Aula_02/ex_04.py
@@ -0,0 +1,9 @@
+#Dada a lista notas = [7.5, 8.0, 5.5, 9.0], calcule e mostre a média das notas.o.
+
+notas = [7.5, 8.0, 5.5, 9.0, 10]
+
+# soma_notas = notas[0]+notas[1]+notas[2]
+
+soma_notas = 
+
+print(soma_notas)
