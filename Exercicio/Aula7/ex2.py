211438f374f261b91bfbbb9e62948afdfb5a5739
Lorenzo-lopes <lorenzolopes.dev@gmail.com>
2025-11-17 16:37:42 -0300

    Correção ex2-aula7

--- Exercicio/Aula7/ex2.py
+++ Exercicio/Aula7/ex2.py
@@ -4,8 +4,7 @@ lado3 = int(input("Digite o tamanho do outro lado"))
 
 if lado1 == lado2 ==lado3:
     print("Equilatero")
-
 elif lado1 == lado2 or lado1 == lado3 or lado2 == lado3:
     print('isoceles')
 elif lado1!= lado2 !=lado3:
-    print("escalo")
+    print("escaleno")
