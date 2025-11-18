336d73d63bebf98103a2cd8ac3930aa47de32d95
Lorenzo-lopes <lorenzolopes.dev@gmail.com>
2025-11-18 09:55:15 -0300

    Add verificação de formação do triangulo

--- Exercicio/Aula7/ex2.py
+++ Exercicio/Aula7/ex2.py
@@ -1,10 +1,13 @@
 lado1= int(input("Digite o tamanaho em cm: "))
 lado2 = int(input("Digite o tamanho do outro lado"))
 lado3 = int(input("Digite o tamanho do outro lado"))
-
-if lado1 == lado2 ==lado3:
-    print("Equilatero")
-elif lado1 == lado2 or lado1 == lado3 or lado2 == lado3:
-    print('isosceles')
-elif lado1!= lado2 !=lado3:
-    print("escaleno")
+if lado1+lado2 < lado3 or lado1+lado3 < lado2 or lado2+lado3 < lado1:
+     print("Os lados informados nao podem formar um triangulo.")
+else:
+    if lado1 == lado2 ==lado3:
+        print("Equilatero")
+    elif lado1 == lado2 or lado1 == lado3 or lado2 == lado3:
+        print('isosceles')
+    elif lado1!= lado2 !=lado3:
+        print("escaleno")
+   
