bb2025f32e6beaa90a84072223226fd0132d678c
Lorenzo-lopes <lorenzolopes.dev@gmail.com>
2025-11-22 15:36:04 -0300

    Correção de digitação

--- Exercicio/Aula7/ex2.py
+++ Exercicio/Aula7/ex2.py
@@ -1,11 +1,8 @@
-lado1= int(input("Digite o tamanaho em cm: "))
-lado2 = int(input("Digite o tamanho do outro lado"))
-lado3 = int(input("Digite o tamanho do outro lado"))
-if lado1+lado2 < lado3 or lado1+lado3 < lado2 or lado2+lado3 < lado1:
+lado1= int(input("Digite o tamanho em cm: "))
+lado2 = int(input("Digite o tamanho do outro lado: "))
+lado3 = int(input("Digite o tamanho do outro lado: "))
+if lado1+lado2 <= lado3 or lado1+lado3 <= lado2 or lado2+lado3 <= lado1:
      print("Os lados informados nao podem formar um triangulo.")
-
-
-
 
 else:
     if lado1 == lado2 ==lado3:
@@ -13,5 +10,5 @@ else:
     elif lado1 == lado2 or lado1 == lado3 or lado2 == lado3:
         print('Isosceles')
     elif lado1!= lado2 !=lado3:
-        print("Escaleno")
+        ...
    
