61085e86eadc1f17daec1d7d7bc429d0cf9d5f19
Lorenzo-Lopes <lorenzolopes.dev@gmail.com>
2025-11-23 00:46:35 -0300

    codigo teste  -  copilot

--- Exercicio/desafios/Jogo_batalha/main.py
+++ Exercicio/desafios/Jogo_batalha/main.py
@@ -8,11 +8,11 @@ class jogador():
         self.ataque =   ataque
         self.pocao =    pocao
 
-player1 =jogador(100,100,15,3)
-player2 =jogador(100,100,15,3)
+p1 =jogador(100,100,15,3)
+p2=jogador(100,100,15,3)
 
 def start_combat():
-    while player1.vida>0 or player2.vida>1:
+    while p1.vida>0 or p2.vida>1:
         print('Jogador 1: Digite 1-ataque | 2-Defender| 3- Curar')
         acaoP1 = input('Digite sua Ação: ')
         os.system('clear')
@@ -24,4 +24,81 @@ def start_combat():
             sleep(0.5)
             print("\n")
         
-start_combat()
+def p1_action():
+    op1 = int(input("Qual vai ser sua ação?"))
+    os.system('clear')
+
+def p2_action():
+    op2 = input(input("Qual vai ser sua ação?"))
+    os.system('clear')
+
+while p1.vida or p2.vida: 
+    op1= p1_action()
+    op2=p2_action()
+
+    if op1 == 1 and op2 == 1:
+        p1.vida -= p2.ataque
+        p2.vida -= p1.ataque
+        print(f'Jogador 1 atacou o Jogador 2 e causou {p1.ataque} de dano')
+        print(f'Jogador 2 atacou o Jogador 1 e causou {p2.ataque} de dano')
+
+    elif op1 == 1 and op2 == 2:
+        dano = p1.ataque - p2.defesa
+        if dano < 0:
+            dano = 0
+        p2.vida -= dano
+        print(f'Jogador 1 atacou o Jogador 2 e causou {dano} de dano')  
+    elif op1 == 2 and op2 == 1:
+        dano = p2.ataque - p1.defesa
+        if dano < 0:
+            dano = 0
+        p1.vida -= dano
+        print(f'Jogador 2 atacou o Jogador 1 e causou {dano} de dano')
+    
+    elif op1 == 2 and op2 == 2:
+        print('Ambos os jogadores defenderam. Nenhum dano foi causado.')
+    elif op1 == 3 and op2 == 1:
+        if p1.pocao > 0:
+            p1.vida += 20
+            p1.pocao -= 1
+            print('Jogador 1 usou uma poção e recuperou 20 de vida.')
+        p1.vida -= p2.ataque
+        print(f'Jogador 2 atacou o Jogador 1 e causou {p2.ataque} de dano')
+    elif op1 == 1 and op2 == 3:
+        if p2.pocao > 0:
+            p2.vida += 20
+            p2.pocao -= 1
+            print('Jogador 2 usou uma poção e recuperou 20 de vida.')
+        p2.vida -= p1.ataque
+        print(f'Jogador 1 atacou o Jogador 2 e causou {p1.ataque} de dano')
+    elif op1 == 3 and op2 == 2:
+        if p1.pocao > 0:
+            p1.vida += 20
+            p1.pocao -= 1
+            print('Jogador 1 usou uma poção e recuperou 20 de vida.')
+        print('Jogador 2 defendeu. Nenhum dano foi causado.')
+    elif op1 == 2 and op2 == 3:
+        if p2.pocao > 0:
+            p2.vida += 20
+            p2.pocao -= 1
+            print('Jogador 2 usou uma poção e recuperou 20 de vida.')
+        print('Jogador 1 defendeu. Nenhum dano foi causado.')
+    elif op1 == 3 and op2 == 3:
+        if p1.pocao > 0:
+            p1.vida += 20
+            p1.pocao -= 1
+            print('Jogador 1 usou uma poção e recuperou 20 de vida.')
+        if p2.pocao > 0:
+            p2.vida += 20
+            p2.pocao -= 1
+            print('Jogador 2 usou uma poção e recuperou 20 de vida.')
+    print(f'Vida Jogador 1: {p1.vida} | Vida Jogador 2: {p2.vida}')
+    print(f'Poções Jogador 1: {p1.pocao} | Poções Jogador 2: {p2.pocao}')
+    print('-----------------------------------')
+    if p1.vida <= 0:
+        print('Jogador 2 Venceu!')
+        break
+    elif p2.vida <= 0:
+        print('Jogador 1 Venceu!')
+        break
+    
