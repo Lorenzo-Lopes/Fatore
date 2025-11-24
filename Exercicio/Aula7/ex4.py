c7a6fb8ca1d2b69e8256e5aee22d99709ad72a16
Lorenzo-lopes <lorenzolopes.dev@gmail.com>
2025-11-24 18:35:29 -0300

    remoção de print redundante

--- Exercicio/Aula7/ex4.py
+++ Exercicio/Aula7/ex4.py
@@ -3,8 +3,6 @@ numero = int(input("Digite um numero: "))
 centena = numero // 100
 dezena = (numero //10 )%10
 unidade = numero %10
-print(centena,dezena,unidade)
-
 numero_invertido= (unidade*100)+(dezena*10)+centena
 
 print(numero_invertido)
