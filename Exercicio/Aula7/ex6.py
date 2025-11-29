463fc46bc0fcba6f9ca572710e745208985fb9a5
Lorenzo-lopes <lorenzolopes.dev@gmail.com>
2025-11-29 07:51:41 -0300

    correção  de exibição

--- Exercicio/Aula7/ex6.py
+++ Exercicio/Aula7/ex6.py
@@ -4,4 +4,5 @@ qtd_sequencia= int(input('Quantos numero da sequencia de Fibonacci deseja ver? '
 for n in range(qtd_sequencia):
 
     fibonacci.append(fibonacci[-1]+fibonacci[-2])
-    print(fibonacci[n])
+    print(fibonacci[-1])
+    
