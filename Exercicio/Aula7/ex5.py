985aed48267ec84bdc5804b12a184912b1cfd187
Lorenzo-Lopes <lorenzolopes.dev@gmail.com>
2025-12-04 19:31:06 -0300

    add ex

--- Exercicio/Aula7/ex5.py
+++ Exercicio/Aula7/ex5.py
@@ -1,16 +1,16 @@
-# palavra= input("Digite uma palavra: ")
-
-# invertida = []
-# str_invertida=''
-# for letra in palavra:
-#     invertida.insert(0,letra)
-# for letra in invertida:
-#     str_invertida+=letra
-
 palavra= input("Digite uma palavra: ")
-str_invertida = palavra[::-1]
+
+invertida = []
+str_invertida=''
+for letra in palavra:
+    invertida.insert(0,letra)
+for letra in invertida:
+    str_invertida+=letra
+
+# palavra= input("Digite uma palavra: ")
+# str_invertida = palavra[::-1]
     
-if palavra == str_invertida:
-    print(f'{palavra} é um PALINDROMO')
-else:
-    print("Nao é um palindromo.")
+# if palavra == str_invertida:
+#     print(f'{palavra} é um PALINDROMO')
+# else:
+#     print("Nao é um palindromo.")
